c7f5228c7c322755a7e50d356c816c304b468345
Sourabh Shirhatti <soshir@microsoft.com>
2018-06-05 14:55:35 -0700

    Initial commit

---
+++ src/TestMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
