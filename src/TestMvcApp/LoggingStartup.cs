906edc31fda6c3bf92f566e5797f774cb3b6d329
Sourabh Shirhatti <soshir@microsoft.com>
2018-06-05 16:46:53 -0700

    Add event source logger

---
+++ src/TestMvcApp/LoggingStartup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[assembly: HostingStartup(typeof(TestMvcApp.LoggingStartup))]
+
+namespace TestMvcApp
+{
+    public class LoggingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddEventSourceLogger();
+            });
+        }
+    }
+}
